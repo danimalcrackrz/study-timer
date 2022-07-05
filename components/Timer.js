deabe8b37464b492bad8ce709c6fab09e599e907
Dan Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2022-07-05 13:27:09 -0500

    -added sass/react icons deps -added Layout/Timer Components -removed some nextjs bp code

---
+++ components/Timer.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react"
+import { BsFillArrowDownSquareFill } from "react-icons/bs"
+import { BsFillArrowUpSquareFill } from "react-icons/bs"
+
+const Timer = () => {
+  const [isRunning, setIsRunning] = useState(false)
+  const [time, setTime] = useState(60)
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setTime((oldTime) => {
+          const newTime = oldTime - 1
+          if (newTime === 0) {
+            setIsRunning(false)
+          }
+          return newTime
+        })
+      }, 1000)
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  }, [isRunning])
+
+  return (
+    <div className='timer'>
+      <div className='timer-container'>
+        {time}
+        <div className='timer-adjustment'>
+          <BsFillArrowUpSquareFill onClick={() => setTime(time + 1)} />
+          <BsFillArrowDownSquareFill onClick={() => setTime(time - 1)} />
+        </div>
+      </div>
+      <div className='timer-contents'>
+        <button className='timer-btn' onClick={() => setIsRunning(!isRunning)}>
+          {isRunning ? "Pause Timer" : "Start Timer"}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Timer
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout"
+import "../styles/globals.css"
+import "../styles/App.scss"
+import "../styles/Timer.scss"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
